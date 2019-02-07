37a3a3cfc513b09cb84bd13a7c1ec7bed6b88ae8
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-07 17:38:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,3 +11,13 @@ def oxfort_comma(array)
     return array[0..-2].join(', ') + ", and " + array[-1]
   end
 end
+
+def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+end 
